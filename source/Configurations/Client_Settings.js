0c2a6292c68169d95eb7783385479cb10c461865
iTheWUULN <96634321+iTheWUULN@users.noreply.github.com>
2024-03-27 23:48:50 +0300

    Update Client_Settings.js

--- source/Configurations/Client_Settings.js
+++ source/Configurations/Client_Settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-    botToken: "MTIyMjE5MTUyMDUyMjk2MDkzNw.Gc2HHY.DfJ7vXodwWGiaHDwOdeBmSCz-m4yTuru4xxWpI",
+    botToken: "MTIyMjE5MTUyMDUyMjk2MDkzNw.G7T4Vd.cpma94s09tcGi3n7rlz5Y_uVO_cP3kRHh3WzII",
     clientOwner: ["795204246046769182"],
     prefix: ["."],
 
